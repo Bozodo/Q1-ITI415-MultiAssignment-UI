71ecc55eeab405a1c4941993d2cab6a89213d28a
Bozodo <aquinojt.1@gmail.com>
2020-05-13 01:30:50 -0400

    Added FPS_exiter to exit all the FPS controller levels.
    You can escape to return to the main scene switcher. Also unlocks the mouse cursor from MouseLook.cs.

---
+++ Assets/FPS_exiter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FPS_exiter : MonoBehaviour
+{
+    public UnityStandardAssets.Characters.FirstPerson.MouseLook MSController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MSController.SetCursorLock(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            MSController.SetCursorLock(false);
+            ReturnToMainScene();
+        }
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void ReturnToMainScene()
+    {
+        SceneManager.LoadScene("scene-MainMenu");
+    }
+}
