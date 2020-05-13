22ff42ad9c3cb4bd4a535a8a9c16364e3a6973fd
Bozodo <aquinojt.1@gmail.com>
2020-05-13 00:07:19 -0400

    Imported ProBuilder_Map and installed ProBuilder into the project

---
+++ Assets/Scripts/FPS_Collector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FPS_Collector : MonoBehaviour
+{
+    
+    private int count;
+    public Text countText;
+    public Text winText;
+    public AudioClip dingSound;
+    private AudioSource AudioSource;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            AudioSource.clip = dingSound;
+            AudioSource.Play();
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 3)
+        {
+            winText.text = "Close Enough";
+        }
+    }
+}
